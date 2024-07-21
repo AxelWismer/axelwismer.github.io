6a77217d22a462fa55f6491edd393280a8224499
AxelWismer <mmowismer@outlook.com>
2024-07-21 17:03:52 -0300

    General settings and about section

--- _dev/convert_images.sh
+++ _dev/convert_images.sh
@@ -1,11 +1,11 @@
-convert assets/img/blog/patcat-architecture.png -resize 960x720^ -gravity center -extent 960x720 assets/img/blog/patcat-architecture@1x.png
-
 # Resize a image to the proper proportion
 convert assets/img/blog/patcat-architecture.png -resize 960x720 -background transparent -gravity center -extent 960x720 assets/img/blog/patcat-architecture@0,5x.png
 
-# Resize the image to smaller values
-convert assets/img/blog/patcat-architecture@0,5x.png -resize 50% -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB assets/img/blog/patcat-architecture@0,25x.png
+# Resize the image to smaller values (Without removing quality)
+convert assets/img/blog/patcat-architecture@0,5x.png -resize 50% -sampling-factor 4:2:0 -interlace JPEG -colorspace RGB assets/img/blog/patcat-architecture@0,25x.png
 
-convert assets/img/blog/patcat-architecture.png -resize 50% -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB assets/img/blog/patcat-architecture@0,5x.png
-convert assets/img/blog/patcat-architecture.png -resize 25% -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB assets/img/blog/patcat-architecture@0,25x.png
-convert assets/img/blog/patcat-architecture.png -resize 12.5% -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB /assets/img/blog/patcat-architecture@0,125x.png
+# Remove background
+convert assets/img/logo.png -fuzz 20% -transparent black result.png
+
+# Convert to ico
+convert assets/img/2.png -resize 140x140 -gravity center  assets/img/favicon.ico
--- _featured_categories/blog.md
+++ _featured_categories/blog.md
@@ -3,15 +3,14 @@
 layout: list
 
 # The title of the tag's page.
-title: Example
+title: Blog
 
 # The name of the tag, used in a post's front matter (e.g. tags: [<slug>]).
-slug: example
+slug: blog
 
 # (Optional) Write a short (~150 characters) description of this featured tag.
 description: >
-  This is a featured category, which have their own page.
-  Check out `_featured_tags/example.md` to learn how to create your own.
+   I created this page to document my projects on the areas of frontend, backend, cloud, cryptography and smart contracts. I also like to research about new technologies and solve coding challenges.
 
 # (Optional) You can disable grouping posts by date.
 # no_groups: true
