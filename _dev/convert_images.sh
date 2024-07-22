dec17a7838885c55c57a1ce0489b9fe7f8fa13c7
AxelWismer <mmowismer@outlook.com>
2024-07-22 15:02:48 -0300

    Added SEO config and icons

--- _dev/convert_images.sh
+++ _dev/convert_images.sh
@@ -7,5 +7,18 @@ convert assets/img/blog/patcat-architecture@0,5x.png -resize 50% -sampling-facto
 # Remove background
 convert assets/img/logo.png -fuzz 20% -transparent black result.png
 
+
+# Create icon
 # Convert to ico
-convert assets/img/2.png -resize 140x140 -gravity center  assets/img/favicon.ico
+convert assets/icons/icon.png -resize 140x140 -gravity center assets/icons/favicon.ico
+
+
+## Convert to all the sizes
+convert assets/icons/icon.png -resize 512x512 -background transparent -gravity center assets/icons/icon-512x512.png
+convert assets/icons/icon.png -resize 384x384 -background transparent -gravity center assets/icons/icon-384x384.png
+convert assets/icons/icon.png -resize 192x192 -background transparent -gravity center assets/icons/icon-192x192.png
+convert assets/icons/icon.png -resize 152x152 -background transparent -gravity center assets/icons/icon-152x152.png
+convert assets/icons/icon.png -resize 144x144 -background transparent -gravity center assets/icons/icon-144x144.png
+convert assets/icons/icon.png -resize 96x96 -background transparent -gravity center assets/icons/icon-96x96.png
+convert assets/icons/icon.png -resize 72x72 -background transparent -gravity center assets/icons/icon-72x72.png
+
